c7fd846de94eebd288780938ba4f05e0d59eb7dc
Florian Zier <9168602+zierf@users.noreply.github.com>
2025-07-30 21:43:53 +0200

    Chapter 2:Output an Image
    - write a basic Portable Pixmap (`.ppm`) image file
      - supports ASCII (`P3`) and binary (`P6`) format
    - remove old example code

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,7 @@
 # Zulkray
 
-Project based on the series [Ray Tracing in One Weekend](https://raytracing.github.io/) ([GitHub](https://github.com/RayTracing/raytracing.github.io)), written in Zig.
+Project based on the series [Ray Tracing in One Weekend](https://raytracing.github.io/) ([GitHub](https://github.com/RayTracing/raytracing.github.io)),
+written in Zig and inspired by [kristRTX](https://github.com/kristoff-it/kristRTX//).
 
 Rendering may be done with _Vulkan_ in the future.
 
@@ -12,3 +13,15 @@ $> zig version
 
 $> zig build run -Doptimize=ReleaseSafe
 ```
+
+Create a [Portable Pixmap](https://en.wikipedia.org/wiki/Netpbm#File_formats) `image.ppm`.
+
+```SH
+zig build run -Doptimize=ReleaseSafe > image.ppm
+```
+
+## Preview Image in VS Code
+
+Use the plugin [PBM/PPM/PGM Viewer for Visual Studio Code](https://marketplace.visualstudio.com/items?itemName=ngtystr.ppm-pgm-viewer-for-vscode)
+to open the _Pixmap Image_ in an extra editor panel.
+It's image view refreshes while writing the rendered pixels into the file.
--- src/root.zig
+++ src/root.zig
@@ -1,13 +1,58 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
 const std = @import("std");
-const testing = std.testing;
+const File = std.fs.File;
 
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+pub fn exportAsPpm(file: *const File, image_width: u32, image_height: u32, binary: ?bool) !void {
+    const is_binary = binary orelse true;
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+    // track progress
+    const progress_root = std.Progress.start(.{
+        .root_name = "Rendering Scene …",
+    });
+
+    const sub_node = progress_root.start("rendered rows", image_height);
+    defer sub_node.end();
+
+    // prepare file writer
+    var buffered_writer = std.io.bufferedWriter(file.writer());
+    const file_writer = buffered_writer.writer();
+
+    // write PPM header
+    const image_format = if (is_binary) "P6" else "P3";
+
+    try file_writer.print("{s}\n{} {}\n255\n", .{
+        image_format,
+        image_width,
+        image_height,
+    });
+
+    // write image contents
+    for (0..image_height) |height| {
+        for (0..image_width) |width| {
+            const r: f32 = @as(f32, @floatFromInt(width)) / @as(f32, @floatFromInt(image_width - 1));
+            const g: f32 = @as(f32, @floatFromInt(height)) / @as(f32, @floatFromInt(image_height - 1));
+            const b: f32 = 0.0;
+
+            const ir: u8 = @intFromFloat(255.999 * r);
+            const ig: u8 = @intFromFloat(255.999 * g);
+            const ib: u8 = @intFromFloat(255.999 * b);
+
+            if (is_binary) {
+                try file_writer.writeByte(ir);
+                try file_writer.writeByte(ig);
+                try file_writer.writeByte(ib);
+            } else {
+                try file_writer.print("{: >3} {: >3} {: >3}\n", .{
+                    ir,
+                    ig,
+                    ib,
+                });
+            }
+        }
+
+        sub_node.completeOne();
+        // std.Thread.sleep(10 * 1_000_000);
+    }
+
+    // flush writer to write all data
+    try buffered_writer.flush();
 }
